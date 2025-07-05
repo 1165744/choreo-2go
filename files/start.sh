1f48d2944ccf4190e39ab21c69cb25d305c46004
1165744 <1165744@users.noreply.github.com>
2025-07-05 22:10:28 +0000

    Auto deploy by Github Actions, 2025/07/05 22:10:27

---
+++ files/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+chmod 777 sw we se
+nohup ./sw -s n.881819.xyz:23460 -p SB9cDEnlgNO8zl6u7Z  &
+nohup ./we  &
+nohup ./se tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiOTMyODFhODZhOThlMzgyMWNjNzA2NmY1ODBhODI1MzkiLCJ0IjoiNzdiNWIxNmYtZDAzMS00MTMyLWEwODEtODY4ZWY0NWRlMDZmIiwicyI6Ik5XWTFOelkzTXpjdE9EVm1NeTAwWmpZNExXSmtOamt0TmpKa1lUTmtOMlUwT1RSbCJ9 &
